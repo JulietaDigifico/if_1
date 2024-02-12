44d53ca2a87f47418f9a47a4a7201a744b1b2fcc
Julieta <julieta.digifico@gmail.com>
2024-02-12 05:51:57 -0300

    1 ejercicio if

---
+++ app.py
@@ -0,0 +1,48 @@
+import tkinter
+from tkinter.messagebox import showinfo as alert
+from tkinter.messagebox import askyesno as question
+from tkinter.simpledialog import askstring as prompt
+import customtkinter
+
+'''
+nombre: Julieta
+apellido: Digifico
+
+---
+Ejercicio: if_01
+---
+Enunciado:
+Al presionar el botón 'Mostrar', se deberá obtener el contenido de la caja de texto txt_edad,
+transformarlo en número, si coincide con el valor 18, mostrar el mensaje “Usted tiene 18 años” utilizando el Dialog Alert.
+'''
+
+class App(customtkinter.CTk):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.title("UTN FRA")
+
+        self.label1 = customtkinter.CTkLabel(master=self, text="Edad")
+        self.label1.grid(row=0, column=0, padx=20, pady=10)
+        
+        self.txt_edad = customtkinter.CTkEntry(master=self)
+        self.txt_edad.grid(row=0, column=1)
+                
+        self.btn_mostrar = customtkinter.CTkButton(master=self, text="Mostrar", command=self.btn_mostrar_on_click)
+        self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
+
+
+    def btn_mostrar_on_click(self):
+        edad_text = self.txt_edad.get()
+        print("edad ingresada", edad_text)
+        edad = int(edad_text)
+        if edad == 18:
+         alert("mensaje", "usted tiene 18 años")
+        else: 
+         alert("mensaje", "usted no tiene 18 años")
+      
+if __name__ == "__main__":
+    app = App()
+    app.geometry("300x300")
+    app.mainloop()
